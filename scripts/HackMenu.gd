6d8d2f0a3b1f03a410cc686b402ee7db84036996
Thomsonn123 <thexremu@gmail.com>
2022-12-06 16:16:24 +0100

    hack menu

---
+++ scripts/HackMenu.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var Time = 2
+var progressStep = 10
+var curStep = 0
+
+func startHacking():
+	var ProgressTimer = Time / 100
+	$HacingTimer.wait_time = ProgressTimer
+
+func _on_HacingTimer_timeout():
+	curStep += 1
--- scripts/Monitor.gd
+++ scripts/Monitor.gd
@@ -1,10 +1,6 @@
 extends Area2D
 
 
-
 func _on_Monitor_body_entered(body:Node):
 	if body.name == "Player":
 		body.walkedInMonitor(self.get_path(), self.name)
-
-func sendAction():
-	pass
