c056d54b67b2ddd68a6c54373f3aab16ac852a55
Thomsonn123 <thexremu@gmail.com>
2022-12-07 16:30:42 +0100

    hacking menu update

--- scripts/HackMenu.gd
+++ scripts/HackMenu.gd
@@ -5,8 +5,16 @@ var progressStep = 10
 var curStep = 0
 
 func startHacking():
+	$HacingTimer.start()
 	var ProgressTimer = Time / 100
 	$HacingTimer.wait_time = ProgressTimer
+	
+
+func update():
+	$ProgressBar.value = 10 * curStep
 
 func _on_HacingTimer_timeout():
 	curStep += 1
+	update()
+
+	
