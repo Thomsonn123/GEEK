71c015ec45d1d97591fa94fd819cc1212e27c14f
Thomsonn123 <thexremu@gmail.com>
2023-02-28 01:16:57 +0100

    locals update2

--- scripts/locals.gd
+++ scripts/locals.gd
@@ -6,12 +6,39 @@ export var position2 = Vector2()
 var moveTo
 var walking = load("res://sounds/Grass.mp3")
 
+var animright = load("res://Graphics/SpriteFrames/localsRight.tres")
+var animup = load("res://Graphics/SpriteFrames/localsup.tres")
+var animdown = load("res://Graphics/SpriteFrames/localsDown.tres")
+
 func _ready():
     moveTo = position2
 
 func _physics_process(delta):
     if moveTo == position2 and self.position == position2:
         moveTo = position1
+        changeAnimation(moveTo)
     elif moveTo == position1 and self.position == position1:
         moveTo = position2
+        changeAnimation(moveTo)
     self.position = position.move_toward(moveTo, delta * 100)
+
+func changeAnimation(positionTo):
+    var degrees = rad2deg(self.get_angle_to(positionTo))
+    if degrees > 0:
+        if degrees < 45:
+            $AnimatedSprite.frames = animright
+            $AnimatedSprite.flip_h = false
+        elif degrees > 45 and degrees < 135:
+            $AnimatedSprite.frames = animdown
+        elif degrees > 135:
+            $AnimatedSprite.frames = animright
+            $AnimatedSprite.flip_h = true
+        elif degrees < 0:
+            if degrees > -45:
+                $AnimatedSprite.frames = animright
+                $AnimatedSprite.flip_h = false
+            elif degrees < -45 and degrees > -135:
+                $AnimatedSprite.frames = animup
+            elif degrees < -135:
+                $AnimatedSprite.frames = animright
+                $AnimatedSprite.flip_h = true
